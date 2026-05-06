3546581c3c1e050604e5284a69acdf435136e72d
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2026-05-06 13:06:33 -0500

    feat(pricing): add pricing images and implement cards with overlay

--- src/app/components/pricing/pricing.ts
+++ src/app/components/pricing/pricing.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HeroVideoModel } from '../../models/hero-video/hero-video.model';
+import { HeroVideoService } from '../../services/hero-video/hero-video.service';
+import { CommonModule } from '@angular/common';
+import { Herovideo } from '../herovideo/herovideo';
 
 @Component({
   selector: 'app-pricing',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, Herovideo],
   templateUrl: './pricing.html',
   styleUrl: './pricing.scss',
 })
-export class Pricing {}
+export class Pricing {
+
+  heroVideoMedia!: HeroVideoModel;
+
+  heroVideoService = inject(HeroVideoService);
+
+  services = [
+    {
+      title: 'Bolirana',
+      price: '$10.000 / hora',
+      extra: 'Incluye 1 bebida de cortesía (Único servicio sin reserva, dependiendo disponibilidad).',
+      image: 'images/prices/bolirana.webp'
+    },
+    {
+      title: 'Tiro de hacha',
+      price: '$22.000 / hora',
+      extra: 'Incluye 2 bebidas de cortesía',
+      image: 'images/prices/tiroHacha.webp'
+    },
+    {
+      title: 'Tejo',
+      price: '$22.000 / hora',
+      extra: 'Incluye 2 bebidas de cortesía',
+      image: 'images/prices/tejo.webp'
+    },
+    {
+      title: 'Máquina de boxeo',
+      price: '$2.000 - $20.000',
+      extra: 'El valor depende de la cantidad de golpes que desees jugar.',
+      image: 'images/prices/boxeo.webp'
+    },
+  ];
+
+  ngOnInit() {
+    this.heroVideoMedia = this.heroVideoService.getMedia('prices');
+  }
+}
