f4ce901e086f33f6cc8fa91b598a0ddbcb3a0f7a
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2026-05-15 12:28:44 -0500

    feat(button): support FontAwesome icon input in app-button

--- src/app/components/button/button.ts
+++ src/app/components/button/button.ts
@@ -1,12 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
+  imports: [RouterLink, FontAwesomeModule],
   templateUrl: './button.html',
   styleUrl: './button.scss',
 })
 export class Button {
+  @Input() icon?: IconProp;
   @Input() text = '';
   @Input() href?: string;
   @Input() routerLink?: string;
