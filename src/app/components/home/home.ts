ed7a33c97c758d838f95fa76beaa367d8e8aaa0e
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2026-05-15 12:31:18 -0500

    fix(home): restore icon buttons and add new FontAwesome icon props

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -3,17 +3,31 @@ import { HeroVideoModel } from '../herovideo/models/hero-video.model';
 import { HeroVideoService } from '../../services/hero-video/hero-video.service';
 import { Herovideo } from '../herovideo/herovideo';
 import { Button } from "../button/button";
+import { faClock, faStar, faCalendarCheck, faCalendarDays, faClipboardQuestion } from '@fortawesome/free-solid-svg-icons';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Herovideo, Button],
+  imports: [
+    Herovideo,
+    Button,
+    FontAwesomeModule
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-  
+
+  faClock = faClock;
+  faStar = faStar;
+  faCalendarCheck = faCalendarCheck;
+  faCalendarDays = faCalendarDays;
+  faClipboardQuestion = faClipboardQuestion;
+  faWhatsapp = faWhatsapp;
+
   heroVideoMedia!: HeroVideoModel;
 
   heroVideoService = inject(HeroVideoService);
