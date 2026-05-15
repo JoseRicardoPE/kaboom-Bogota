d0626f115d70cb4db7e067fa059001d34b5868a1
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2026-05-14 20:33:05 -0500

    refactor(home): reorganize home section structure and card styles

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,13 +1,14 @@
 import { Component, inject } from '@angular/core';
-import { HeroVideoModel } from '../../models/hero-video/hero-video.model';
+import { HeroVideoModel } from '../herovideo/models/hero-video.model';
 import { HeroVideoService } from '../../services/hero-video/hero-video.service';
 import { Herovideo } from '../herovideo/herovideo';
+import { Button } from "../button/button";
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Herovideo],
+  imports: [Herovideo, Button],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
