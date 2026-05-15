8767424edc712929d3692c1d8a9e869b87bf4e65
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2026-05-14 20:31:48 -0500

    refactor(hero-video): apply BEM structure and improve responsive styles

--- src/app/components/herovideo/herovideo.ts
+++ src/app/components/herovideo/herovideo.ts
@@ -1,6 +1,6 @@
 import { Component, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeroVideoModel } from '../../models/hero-video/hero-video.model';
+import { HeroVideoModel } from './models/hero-video.model';
 import { HeroVideoService } from '../../services/hero-video/hero-video.service';
 import { ɵEmptyOutletComponent } from "@angular/router";
 
