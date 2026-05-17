d1a977763b6faaf933b866b2586a21fd570d33e8
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2026-05-17 18:55:46 -0500

    refactor(gallery): optimize image tracking alt texts and lazy loading

--- src/app/components/gallery/gallery.ts
+++ src/app/components/gallery/gallery.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { GalleryModel } from './models/gallery-model';
 import { Button } from "../button/button";
 
@@ -8,12 +8,13 @@ import { Button } from "../button/button";
   imports: [Button],
   templateUrl: './gallery.html',
   styleUrl: './gallery.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Gallery {
   images: GalleryModel[] = [
     {
       image: 'images/gallery/clientes_gallery_1.jpg',
-      alt: 'Grupo de personas celebrando',
+      alt: 'Grupo de amigos celebrando',
     },
     {
       image: 'images/gallery/clientes_gallery_2.jpg',
@@ -21,27 +22,27 @@ export class Gallery {
     },
     {
       image: 'images/gallery/clientes_gallery_3.jpg',
-      alt: 'Grupo de personas celebrando',
+      alt: 'amigos celebrando',
     },
     {
       image: 'images/gallery/clientes_gallery_4.jpg',
-      alt: 'Grupo de personas celebrando',
+      alt: 'Grupo de personas disfrutando',
     },
     {
       image: 'images/gallery/clientes_gallery_5.jpg',
-      alt: 'Grupo de personas celebrando',
+      alt: 'Mujeres celebrando',
     },
     {
       image: 'images/gallery/clientes_gallery_6.jpg',
-      alt: 'Grupo de personas celebrando',
+      alt: 'Grupo de personas en una fiesta',
     },
     {
       image: 'images/gallery/clientes_gallery_7.jpg',
-      alt: 'Grupo de personas celebrando',
+      alt: 'Grupo empresarial celebrando',
     },
     {
       image: 'images/gallery/clientes_gallery_8.jpg',
-      alt: 'Grupo de personas celebrando',
+      alt: 'Grupo de personas celebrando en una fiesta',
     },
   ]
 }
