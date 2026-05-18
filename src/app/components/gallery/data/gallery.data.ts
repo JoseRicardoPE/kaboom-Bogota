1224a8a29156560029fc107b2831e5657f812e10
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2026-05-18 11:09:15 -0500

    refactor(gallery): move gallery images to mock data file

---
+++ src/app/components/gallery/data/gallery.data.ts
@@ -0,0 +1,36 @@
+import { GalleryModel } from '../models/gallery-model';
+
+export const GALLERY_DATA: GalleryModel[] = [
+  {
+    image: 'images/gallery/clientes_gallery_1.webp',
+    alt: 'Grupo de amigos celebrando',
+  },
+  {
+    image: 'images/gallery/clientes_gallery_2.webp',
+    alt: 'Grupo de personas celebrando',
+  },
+  {
+    image: 'images/gallery/clientes_gallery_3.webp',
+    alt: 'amigos celebrando',
+  },
+  {
+    image: 'images/gallery/clientes_gallery_4.webp',
+    alt: 'Grupo de personas disfrutando',
+  },
+  {
+    image: 'images/gallery/clientes_gallery_5.webp',
+    alt: 'Mujeres celebrando',
+  },
+  {
+    image: 'images/gallery/clientes_gallery_6.webp',
+    alt: 'Grupo de personas en una fiesta',
+  },
+  {
+    image: 'images/gallery/clientes_gallery_7.webp',
+    alt: 'Grupo empresarial celebrando',
+  },
+  {
+    image: 'images/gallery/clientes_gallery_8.webp',
+    alt: 'Grupo de personas celebrando en una fiesta',
+  },
+];
--- src/app/components/gallery/gallery.ts
+++ src/app/components/gallery/gallery.ts
@@ -1,6 +1,9 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { GalleryModel } from './models/gallery-model';
+
 import { Button } from "../button/button";
+
+import { GalleryModel } from './models/gallery-model';
+import { GALLERY_DATA } from './data/gallery.data';
 
 @Component({
   selector: 'app-gallery',
@@ -11,38 +14,5 @@ import { Button } from "../button/button";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Gallery {
-  images: GalleryModel[] = [
-    {
-      image: 'images/gallery/clientes_gallery_1.jpg',
-      alt: 'Grupo de amigos celebrando',
-    },
-    {
-      image: 'images/gallery/clientes_gallery_2.jpg',
-      alt: 'Grupo de personas celebrando',
-    },
-    {
-      image: 'images/gallery/clientes_gallery_3.jpg',
-      alt: 'amigos celebrando',
-    },
-    {
-      image: 'images/gallery/clientes_gallery_4.jpg',
-      alt: 'Grupo de personas disfrutando',
-    },
-    {
-      image: 'images/gallery/clientes_gallery_5.jpg',
-      alt: 'Mujeres celebrando',
-    },
-    {
-      image: 'images/gallery/clientes_gallery_6.jpg',
-      alt: 'Grupo de personas en una fiesta',
-    },
-    {
-      image: 'images/gallery/clientes_gallery_7.jpg',
-      alt: 'Grupo empresarial celebrando',
-    },
-    {
-      image: 'images/gallery/clientes_gallery_8.jpg',
-      alt: 'Grupo de personas celebrando en una fiesta',
-    },
-  ]
+  images: GalleryModel[] = GALLERY_DATA;
 }
