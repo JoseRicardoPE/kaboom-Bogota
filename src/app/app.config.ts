a791abc230a5706da7d2de3748bb47deaeb327ab
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2026-05-26 15:27:36 -0500

    feat(seo): enable prerendered routes for improved indexing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,23 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideToastr} from 'ngx-toastr';
+import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(
-      routes, 
+      routes,
       withInMemoryScrolling({
         scrollPositionRestoration: 'top',
         anchorScrolling: 'enabled',
-      })
+      }),
     ),
     provideAnimations(),
     provideToastr(),
-  ]
+    provideClientHydration(withEventReplay()),
+  ],
 };
